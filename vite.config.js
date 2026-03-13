a93e4a7f0831bdbe0db0f1229dfd652cab4e63fd
Nimsika-Bosilu <nbosilu@gmail.com>
2026-03-13 06:03:25 +0530

    port

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: parseInt(process.env.PORT) || 5173,
+    host: true, // Listen on all local IPs
     open: true
+  },
+  preview: {
+    port: parseInt(process.env.PORT) || 5173,
+    host: true,
   },
   define: {
     global: 'globalThis',
